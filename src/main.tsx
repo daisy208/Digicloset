468633599b5ad8701012d291ed8c54fd21e73482
Aditi Singh <217724797+daisy208@users.noreply.github.com>
2025-09-20 22:51:48 +0530

    Update main.tsx
    Signed-off-by: Aditi Singh <217724797+daisy208@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -32,3 +32,25 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        success: {
+          style: { background: "#10B981", color: "#fff" },
+        },
+        error: {
+          style: { background: "#EF4444", color: "#fff" },
+        },
+      }}
+    />
+  </React.StrictMode>
+);
