83b37712c28989d6f2011d818a9d1d9c54a268a6
Aditi Singh <217724797+daisy208@users.noreply.github.com>
2025-09-30 20:20:49 +0530

    Update main.tsx
    Signed-off-by: Aditi Singh <217724797+daisy208@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -60,3 +60,16 @@ import posthog from "posthog-js";
 posthog.init("phc_YOUR_PROJECT_API_KEY", {
   api_host: "https://app.posthog.com",
 });
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+);
